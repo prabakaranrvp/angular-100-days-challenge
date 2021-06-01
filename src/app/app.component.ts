30ada8f80f8e6e15060586e36ab76f441451dfbc
Prabakaran Raja <prabakaranrvp@gmail.com>
2021-06-01 09:01:46 +0530

    Day 1 - Environment Setup, Hello world App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ang-first-app';
+  title = '100 Days Angular Challenge';
 }
